35dae47fa0cd0c43cdba906d428d4042aaf29e64
camila-osores <camilaosoresa@gmail.com>
2020-02-14 13:53:53 -0300

    boton funcionando de publicar post en database

--- src/lib/view-home.js
+++ src/lib/view-home.js
@@ -1,7 +1,7 @@
 export function home() {
     return `
     <div class="container">
-        <textarea class="post" name="" id="" cols="50" rows="8"></textarea>
+        <textarea class="post" name="" id="post-area" cols="50" rows="8"></textarea>
         <button id="btn-publicar" class="btns">publicar</button>
         <button id="btn-cerrar" class="btns">cerrar sesión</button>
     </div>`
--- src/main.js
+++ src/main.js
@@ -21,26 +21,19 @@ function iniciar() {
     });
 }
 
-
+// Iniciar sesión con Google
 function inicioGoogle() {
     let provider = new firebase.auth.GoogleAuthProvider();
     firebase.auth().signInWithPopup(provider)
         .then(function(result) {
-            // This gives you a Google Access Token. You can use it to access the Google API.
             var token = result.credential.accessToken;
-            // The signed-in user info.
             var user = result.user;
-            // ...
         })
         .catch(function(error) {
-            // Handle Errors here.
             var errorCode = error.code;
             var errorMessage = error.message;
-            // The email of the user's account used.
             var email = error.email;
-            // The firebase.auth.AuthCredential type that was used.
             var credential = error.credential;
-            // ...
         });
 }
 
@@ -53,6 +46,8 @@ function observador() {
             var anonymous = user.isAnonymous;
             var uid = user.uid;
             console.log("Existe usuario activo");
+            document.getElementById('btn-publicar').addEventListener("click", publicar)
+            console.log("funciona boton publicar");
             document.getElementById('btn-cerrar').addEventListener("click", cerrar)
 
         } else {
@@ -73,9 +68,7 @@ let cerrar = () => {
     firebase.auth().signOut()
         .then(function() {
             console.log("Has cerrado sesión")
-        }).catch(function(error) {
-            // An error happened.
-        });
+        }).catch(function(error) {});
 }
 
 let regis = () => {
@@ -119,3 +112,25 @@ function verificar() {
             console.log(error);
         });
 }
+
+// Initialize Cloud Firestore through Firebase
+let db = firebase.firestore();
+
+//Agregar datos a firestore
+function publicar() {
+    let post = document.getElementById('post-area').value;
+
+    db.collection("post").add({
+            posteo: post,
+            // last: "Lovelace",
+            // born: 1815
+        })
+        .then(function(docRef) {
+            console.log("Document written with ID: ", docRef.id);
+            document.getElementById('post-area').value = ' ';
+        })
+        .catch(function(error) {
+            console.error("Error adding document: ", error);
+        });
+}
+publicar();
